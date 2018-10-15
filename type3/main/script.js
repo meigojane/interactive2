77e8a44ae201163d50a4e92b7ab29cff4560d10a
harrison8024 <harrison8024@gmail.com>
2018-10-15 02:30:30 -0700

    kick and born done

--- type3/main/script.js
+++ type3/main/script.js
@@ -1,1 +1,37 @@
-let circle7 =document.querySelector('.circle7')
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('Your document is ready!');
+    circle2();
+    circle7();
+ });
+
+function circle2(){
+    let circle = document.getElementsByClassName("circle2");
+    circle[0].addEventListener("mouseover", kick);
+    circle[0].addEventListener("mouseleave", kickOff);
+}
+
+function kick(){
+    let element = document.getElementById("left_front_leg");
+    element.classList.add("kick");
+}
+
+function kickOff(){
+    let element = document.getElementById("left_front_leg");
+    element.classList.remove("kick");
+}
+
+function circle7(){
+    let circle = document.getElementsByClassName("circle7");
+    circle[0].addEventListener("mouseover", born);
+    circle[0].addEventListener("mouseleave", unborn);
+}
+
+function born(){
+    let element = document.getElementById("lil_ele");
+    element.classList.remove("hidden");
+}
+
+function unborn(){
+    let element = document.getElementById("lil_ele");
+    element.classList.add("hidden");
+}
