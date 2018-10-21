e8a1dd1e065a4eed78f4fa5b78bbd5fd6fd62885
janechen <janechen@cca.edu>
2018-10-21 01:31:11 -0700

    actions

--- type3/main/script.js
+++ type3/main/script.js
@@ -1,7 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log('Your document is ready!');
+    circleEventListener("circle1", "eye1", "opening", "hover");
     circleEventListener("circle2", "left_front_leg", "kick", "hover");
-    circleEventListener("circle7", "lil_ele", "show", "toggle");
+    circleEventListener("circle2", "dust_s", "kick_s", "hover");
+    circleEventListener("circle2", "dust_m", "kick_m", "hover");
+    circleEventListener("circle4", "head", "hidden", "hover");
+    circleEventListener("circle4", "head2", "show", "hover");
+    // circleEventListener("circle5", "ele_head", "shake", "hover");
+    circleEventListener("circle5", "elephant", "hidden", "hover");
+    circleEventListener("circle5", "running", "show", "hover");
+    circleEventListener("circle6", "elephant", "push2", "hover");
+    circleEventListener("circle6", "push", "show", "hover");
+    circleEventListener("circle6", "push", "pushing", "hover");
+    circleEventListener("circle6", "elephant", "push2", "hover");
+    circleEventListener("circle7", "lil_ele", "show", "hover");
+    circleEventListener("circle7", "lil_one", "show", "hover");
+    circleEventListener("circle8", "ear", "waving", "hover");
+    circleEventListener("circle3", "tail", "hidden", "hover");
+    circleEventListener("circle3", "tail_2", "show", "hover");
     // circle2();
     // circle7();
 });
