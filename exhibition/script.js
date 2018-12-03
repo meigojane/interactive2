9409746f50afa111f1ad5e44a15cdf12fbe98a3e
janechen <janechen@cca.edu>
2018-12-03 14:03:02 -0800

    exhibition

--- exhibition/script.js
+++ exhibition/script.js
@@ -2,32 +2,54 @@
 $(document).ready(function () {
   //your code here
 
-$('.fire').click(function() {
-    $(this).addClass('hide');
-});
-$('.glass').click(function() {
-    $('.glass').addClass('hide');
-});
+	$('.bush').click(function() {
+	    $('.bush').addClass('hide');
+	    $('.bkg').addClass('hide');
+	    $('.fire').removeClass('hide');
+	});
 
-$('.bush').click(function() {
-    $('.glass').removeClass('hide');
-    $('.fire').removeClass('hide');
-});
+	$('.fire').click(function() {
+	    $('.fire').addClass('hide');
+	    $('.bkg1').addClass('hide');
+	    $('.glass').removeClass('hide');
+	    
+	});
 
+	
+	$('.glass').click(function() {
+	    $('.bush').removeClass('hide');
+	    $('.bkg').removeClass('hide');
+	    $('.glass').addClass('hide');
+	    $('.bkg1').removeClass('hide');
+
+	});
 
 });
 
 let easel = document.querySelector('#outerEasel')
 
 
+easel.addEventListener('click',click)
 
-function () click{
-let newEasel = document.createElement('div')
-newEasel.classList.add('scaleDown')
-newEasel.innerHTML = '<img class="frame1" src="assets/web_window.jpg>'
+let input = document.querySelector('#user-caption')
 
-easel.appendChild(newEasel)
 
-easel = newEasel
+
+function click(){
+	let newEasel = document.createElement('div')
+	newEasel.classList.add('scaleDown')
+	newEasel.innerHTML = '<img class="frame1" src="assets/web_window.jpg">'
+
+	easel.appendChild(newEasel)
+
+	easel = newEasel
 }
 
+function doAPrint() {
+
+	let name = document.createElement('h1')
+	name.innerText = input.value
+
+	document.body.appendChild(name)
+    window.print();
+}
